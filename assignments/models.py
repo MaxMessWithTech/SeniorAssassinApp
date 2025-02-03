2efae8c5f919cb9a22de03f954c861129c784d4d
Max Miller <max@benmiller.com>
2025-02-03 08:35:14 -0600

    added gitignore

--- assignments/models.py
+++ assignments/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=200)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+
+class Round(models.Model):
+    index = models.IntegerField(default=0)
+
+
+class Match(models.Model):
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+    team_1 = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team_2 = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    start_date = 
+
+
