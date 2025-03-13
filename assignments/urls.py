501c5a52a5c0365da99a073f0263fb1a1f56cc5d
Max Miller <max@benmiller.com>
2025-03-13 15:49:30 -0500

    added labels

--- assignments/urls.py
+++ assignments/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("eliminate-participant", views.eliminateParticipant, name="eliminate-participant"),
     path("cleanup_round", views.cleanup_round, name="cleanup_round"),
     path("admin-control", views.adminControl, name="admin-control"),
-    path("accounts/login", views.admin_login_view, name="admin-login"),
+    path("accounts/login/", views.admin_login_view, name="admin-login"),
     
     # path("participant-autocomplete/$", views.ParticipantAutocomplete.as_view(), name="participant-autocomplete"),
     # path("target-autocomplete/$", views.TargetAutocomplete.as_view(), name="target-autocomplete"),
