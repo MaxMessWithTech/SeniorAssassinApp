aa7a9ee86ce3d88fef18ce1536977714989edf87
Max Miller <max@benmiller.com>
2025-05-03 01:09:37 -0500

    ruewnfwiunfe

--- assignments/views.py
+++ assignments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, JsonResponse
 from django.template import loader
 from django.urls import reverse
 from django.utils import timezone
@@ -678,5 +678,5 @@ def revertThingsFromToday(request):
 	# 	change.history.prev_record()
 	# 	change.instance.save()
 
-	return recent_changes
+	return JsonResponse(recent_changes)
 
